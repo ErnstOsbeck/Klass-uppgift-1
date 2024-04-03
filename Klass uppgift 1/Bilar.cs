ed08c5647aabe0a5909db46be15af13c20a4edb4
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-04-03 10:25:48 +0200

    Add project files.

---
+++ Klass uppgift 1/Bilar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser
+{
+    internal class Bilar
+    {
+        public string Märke;
+        public string Modell;
+        public string Färg;
+        public string Motor;
+        public string Kraft;
+
+        public void Bil()
+        {
+            Console.Write(Märke);
+            Console.WriteLine(" " + Modell);
+            Console.WriteLine("Färg: " + Färg);
+            Console.WriteLine("Motor: " + Motor);
+            Console.WriteLine("Effekt: " + Kraft);
+        }
+
+    }
+}
---
+++ Klass uppgift 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bilar B1 = new Bilar();
+            B1.Märke = "Ferrari";
+            B1.Modell = "LaFerrari";
+            B1.Färg = "Rosso Corsa";
+            B1.Motor = "6.3L V12 + elektrisk motor/KERS";
+            B1.Kraft = "950hp";
+
+            B1.Bil();
+        }
+    }
+}
